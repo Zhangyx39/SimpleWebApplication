dc92b2c5d0022b183d632aa24adf485e48956eef
Tommy <tommykuaidlaa@gmail.com>
2017-08-28 19:12:14 -0400

    Second part

--- fullstack_template/static/js/App.jsx
+++ fullstack_template/static/js/App.jsx
@@ -1,8 +1,26 @@
 // App.jsx
 import React from "react";
+import Hello from "./Hello";
+import { PageHeader } from "react-bootstrap";
+import HeaderBackgroundImage from '../images/header.jpg';
+
+require('../css/fullstack.css');
+var $ = require('jquery');
 
 export default class App extends React.Component {
 	render () {
-		return <p> Hello React!!!</p>;
+		return (
+			<PageHeader>
+				<div className='header-contents'>
+					{this.addHeaderImg()}
+					<Hello name='Tommy' />
+				</div>
+			</PageHeader>
+		);
+	}
+
+	addHeaderImg() {
+		let headerBg = new Image();
+		headerBg.src = HeaderBackgroundImage;
 	}
 }
